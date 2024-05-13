6c53aab6a097c78aa805b62cb9de14b91df19996
AhmedE16 <167995951+AhmedE16@users.noreply.github.com>
2024-05-13 20:20:30 +0300

    Update DesignCourse.java

--- project/GUI/Classes/Classes/DesignCourse.java
+++ project/GUI/Classes/Classes/DesignCourse.java
@@ -1,10 +1,10 @@
-package application;
+// package application;
 
-public class DesignCourse extends Course implements Duration {
-    public DesignCourse(String title, String description) {
-        super(title, description);
-    }
-    public String course_duration() {
-		return "design course is 6 weeks" ;	
-    }
-}
+// public class DesignCourse extends Course implements Duration {
+//     public DesignCourse(String title, String description) {
+//         super(title, description);
+//     }
+//     public String course_duration() {
+// 		return "design course is 6 weeks" ;	
+//     }
+// }
